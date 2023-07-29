ff202ce15afb19dc42c34d76afbcc5348fdc0d22
Panos <22086296+valavanisp@users.noreply.github.com>
2023-07-29 17:40:16 -0400

    modified logic, playing from a single button

--- javascript.js
+++ javascript.js
@@ -3,10 +3,10 @@ console.log("Hello, Rock, Paper, Scissors!");
 // global variables for the game
 let playerWins = 0;
 let computerWins = 0;
+let roundsPlayed = 0;
 
 // function to get computer selection
 function getComputerChoice() {
-    
     // 0 = rock, 1 = paper, 2 = scissors
     let number = Math.floor(Math.random() * 3);
     let computer = '';
@@ -27,68 +27,55 @@ function getComputerChoice() {
 
 // function to play a round of the game
 function playRound(playerSelection, computerSelection) {
-    
     let result = "";
     switch (playerSelection) {
         case 'rock':
-            if (computerSelection === 'rock')
-            {
+            if (computerSelection === 'rock') {
                 result = "It's a tie!";
             }
-            else if (computerSelection === 'paper')
-            {
+            else if (computerSelection === 'paper') {
                 result = "You lose! Paper beats rock!";
                 computerWins++;
             }
-            else if (computerSelection === 'scissors')
-            {
+            else if (computerSelection === 'scissors') {
                 result = "You win! Rock beats scissors!";
                 playerWins++;
             }
-            else
-            {
+            else {
                 result = "Congrats! You have memory access issues or something! You should probably get that checked out. No, really. I mean you, personally. Not the game. I'm sorry you had to find out this way, but... I couldn't find another way to tell you... You're also adopted.";
             }
             break;
 
             case 'paper':
-            if (computerSelection === 'rock')
-            {
+            if (computerSelection === 'rock') {
                 result = "You win! Paper beats rock!";
                 playerWins++;
             }
-            else if (computerSelection === 'paper')
-            {
+            else if (computerSelection === 'paper') {
                 result = "It's a tie!";
             }
-            else if (computerSelection === 'scissors')
-            {
+            else if (computerSelection === 'scissors') {
                 result = "You lose! Scissors beats paper!";
                 computerWins++;
             }
-            else
-            {
+            else {
                 result = "We should see other people...";
             }
             break;
 
             case 'scissors':
-            if (computerSelection === 'rock')
-            {
+            if (computerSelection === 'rock') {
                 result = "You lose! Rock beats scissors!";
                 computerWins++;
             }
-            else if (computerSelection === 'paper')
-            {
+            else if (computerSelection === 'paper') {
                 result = "You win! Scissors beats paper!";
                 playerWins++;
             }
-            else if (computerSelection === 'scissors')
-            {
+            else if (computerSelection === 'scissors') {
                 result = "It's a tie!";
             }
-            else
-            {
+            else {
                 result = "Hello darkness my old friend...";
             }
             break;
@@ -97,29 +84,58 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
-function game() {
+// modify this function so that the player selection is a function 
+// from a button press, and the button is passed as an arg
+function game(pChoice) {
     // play 5 games
-    for (let i = 0; i < 5; i++)
-    {
-        // get player input
-        let player = prompt("Rock, Paper, or Scissors? (Case doesn't matter. Go wYld.)").toLowerCase();
-        
-        let outcome = playRound(player, getComputerChoice());
+    //for (let i = 0; i < 1; i++) {   // TODO: change back to 5
+        //let player = prompt("Rock, Paper, or Scissors? (Case doesn't matter. Go wYld.)").toLowerCase();
+        // let outcome = playRound(player, getComputerChoice());
+        let outcome = playRound(pChoice, getComputerChoice());
         console.log(outcome);
-    }
-    
-    if (playerWins > computerWins)
-    {
-        console.log("Player wins overall!");
-    }
-    else if (playerWins < computerWins)
-    {
-        console.log("Computer wins overall!");
-    }
-    else
-    {
-        console.log("Overall tie!");
+        roundsPlayed++;
+    //}
+
+    if (roundsPlayed === 5) {
+        console.log(playerWins);
+        console.log(computerWins);
+        if (playerWins > computerWins) {
+            console.log("Player wins overall!");
+        }
+        else if (playerWins < computerWins) {
+            console.log("Computer wins overall!");
+        }
+        else {
+            console.log("Overall tie!");
+        }
+        roundsPlayed = 0;
+        playerWins = 0;
+        computerWins = 0;
     }
 }
 
-game();
+//game();
+
+function getPlayerChoice(e) {
+     console.log(e.target.innerText);
+     //return e.target.innerText;
+     game(e.target.innerText.toLowerCase())
+}
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", getPlayerChoice);
+
+// function buttonClicked(e) {
+//     const button = document.querySelectorAll('.rps-selection');
+//     console.log(button);
+// }
+
+// window.addEventListener('click', buttonClicked);
+
+// const buttons = document.querySelectorAll('button');
+// buttons.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         console.log(button.id);
+//     })
+// });
